2262d0b3d606f324c6ccecde23fca35f0241e8fb
wangyaofeng <wangyaofeng@moseeker.com>
2020-04-08 13:55:27 +0800

    singleton

---
+++ src/main/java/singleton/ParserService.java
@@ -0,0 +1,66 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017 James
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package singleton;
+
+/**
+ * 线程安全、双检查锁的单例模式
+ * <p>
+ * volatile 所修饰的变量可以被看作是一种 “程度较轻的 synchronized ”；
+ * 与 synchronized 块相比，volatile 变量所需的编码较少，并且运行时开销也较少，
+ * 但是它所能实现的功能也仅是 synchronized 的一部分。
+ * <p>
+ * 锁提供了两种主要特性：互斥（mutual exclusion） 和可见性（visibility）。
+ * 互斥即一次只允许一个线程持有某个特定的锁，因此可使用该特性实现对共享数据的协
+ * 调访问协议，这样，一次就只有一个线程能够使用该共享数据。可见性要更加复杂一些，
+ * 它必须确保释放锁之前对共享数据做出的更改对于随后获得该锁的另一个线程是可见的
+ */
+public final class ParserService {
+
+  private static volatile ParserService INSTANCE;
+
+  private ParserService() {
+    // 防止通过反射进行实例化
+    if (null != INSTANCE) {
+      throw new IllegalStateException("该实例已经存在");
+    }
+  }
+
+  public static ParserService getInstance() {
+    // 如果已经被实例化则直接返回该实例
+    if (null == INSTANCE) {
+      // 无法确定其他的线程是否已经完成初始化
+      // 为了确保我们需要锁定一个对象来进行确认
+      synchronized (ParserService.class) {
+        if (null == INSTANCE) {
+          INSTANCE = new ParserService();
+        }
+      }
+    }
+    return INSTANCE;
+  }
+
+  public String parse(String html){
+    return "{'name':'lisa'}";
+  }
+}
---
+++ src/test/java/singleton/ParserServiceTest.java
@@ -0,0 +1,66 @@
+package singleton;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class ParserServiceTest {
+
+    private final Supplier<ParserService> getInstanceSupplier = ()->{
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+        }
+        return ParserService.getInstance();
+    };
+
+    @Test
+    void getInstance() {
+        ParserService parserService = ParserService.getInstance();
+        String parseResult = parserService.parse("<title><title><body>name <p>lisa</p></body>");
+        log.debug(parseResult);
+        assertTrue(parserService instanceof ParserService);
+    }
+
+    /*
+     * 测试在同一线程中的多次调用返回结果是相同的
+     */
+    @Test
+    @DisplayName("同一线程中的多次调用 getInstance")
+    void testMultipleCallsReturnSameObjectInSameThread(){
+        ParserService ps1 = getInstanceSupplier.get();
+        ParserService ps2 = getInstanceSupplier.get();
+        ParserService ps3 = getInstanceSupplier.get();
+        assertSame(ps1,ps2);
+        assertSame(ps1,ps3);
+    }
+
+    @Test
+    @DisplayName("多线程调用 getInstance")
+    public void testMultipleCallsReturnSameObjectInMultipleThread() throws Exception{
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "50");
+
+        int taskNum = 5000;
+        CompletableFuture[] xs = new CompletableFuture[taskNum];
+        for(int i=0;i<taskNum;i++){
+            xs[i] = CompletableFuture.supplyAsync(getInstanceSupplier);
+        }
+
+        CompletableFuture.allOf(xs).join();
+
+        ParserService expectedInstance = getInstanceSupplier.get();
+
+        for(int i=0;i<taskNum;i++){
+            assertSame(expectedInstance, xs[i].get());
+        }
+
+
+    }
+}
